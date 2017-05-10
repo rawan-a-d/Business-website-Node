6ec73e884e5fa21235edc61052a591346953a6ff
Rawan Abou Dehn <rawan.ad7@gmail.com>
2017-05-10 11:08:13 +0200

    moved the HTML files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use('/', express.static('public', {
-	index: 'HTML/contact.html'
+	index: 'contact.html'
 }));
 
 const listener = app.listen(3000, () =>{
